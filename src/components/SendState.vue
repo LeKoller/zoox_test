04c6c18cc232d1344c4733ce741da9b697122f62
Leandro Koller <lekoller@yahoo.com>
2021-04-07 00:06:34 -0300

    feat: Add first component basis.

---
+++ src/components/SendState.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="">
+      <input type="text" name="nameField" placeholder="Nome do estado" />
+      <input type="text" name="abbreviationField" placeholder="Abreviação" />
+      <button>Create!</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 100px;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    align-items: center;
+
+    input {
+      background-color: transparent;
+      outline: none;
+      border: none;
+      border-bottom: 1px solid #1e1e1e;
+      color: #7067ed;
+      width: 40%;
+      font-size: 1.2rem;
+      margin: 20px;
+
+      &:-webkit-autofill,
+      &:-webkit-autofill:hover,
+      &:-webkit-autofill:focus,
+      &:-webkit-autofill:active {
+        background-color: transparent;
+      }
+    }
+
+    button {
+      outline: none;
+      border: none;
+      background-color: #42b983;
+      color: #121212;
+      font-weight: 600;
+      font-size: 1.1rem;
+      padding: 4px 12px 4px 12px;
+      cursor: pointer;
+      transition: all 0.25s ease;
+      margin-top: 24px;
+
+      &:hover {
+        transform: scale(1.05, 1.05);
+      }
+
+      &:active {
+        transform: scale(0.8, 0.8);
+      }
+
+      &:disabled {
+        color: dimgray;
+        transform: scale(1, 1);
+        cursor: default;
+      }
+    }
+  }
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <SendState msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import SendState from "@/components/SendState.vue";
+
+export default {
+  name: "Home",
+  components: {
+    SendState,
+  },
+};
+</script>
